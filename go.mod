2ac6aa6652c5bdcc01773c5b18b400f6287d759b
t1926 <manu.toivanen@gmail.com>
2021-11-11 12:02:26 +0000

    mcommit testi

---
+++ go.mod
@@ -0,0 +1,3 @@
+module go-static-webapp
+
+go 1.17
---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+import (
+	"log"
+	"net/http"
+)
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello from Snippetbox"))
+}
+
+// Add a showSnippet handler function.
+func showSnippet(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display a specific snippet..."))
+}
+
+// Add a createSnippet handler function.
+func createSnippet(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Create a new snippet..."))
+}
+
+func main() {
+	// Register the two new handler functions and corresponding URL patterns with
+	// the servemux, in exactly the same way that we did before.
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet", showSnippet)
+	mux.HandleFunc("/snippet/create", createSnippet)
+	log.Println("Starting server on :4000")
+	err := http.ListenAndServe(":4000", mux)
+	log.Fatal(err)
+}
+	
